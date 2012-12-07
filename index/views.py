62f536d55730f6b8f6f9f6b1524ac5050971ddee
George Hickman <george@ghickman.co.uk>
2012-12-07 14:11:07 +0000

    Dem pep8s.

--- index/views.py
+++ index/views.py
@@ -17,9 +17,7 @@ class HomeView(ListView):
     template_name = 'home.html'
 
     def get_queryset(self):
-        """
-        Shows recent image for every Card Image.
-        """
+        """Shows recent image for every Card Image."""
         return CardImage.objects.order_by('indexcard', '-id').distinct('indexcard')
 
     def get_context_data(self, **kwargs):
