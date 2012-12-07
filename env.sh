0322cdf23cd879a9730f6f4afe634825ef12f6d6
George Hickman <george@ghickman.co.uk>
2012-12-07 14:40:15 +0000

    Add google credentials back to the env file.
    These are specifically for local so we're safe.

--- env.sh
+++ env.sh
@@ -2,5 +2,7 @@ export DEBUG=True
 export DEVELOPMENT_SITE=True
 export EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
 export AWS_STORAGE_BUCKET_NAME='index.incuna.com'
+export GOOGLE_OAUTH2_CLIENT_ID=223063904227.apps.googleusercontent.com
+export GOOGLE_OAUTH2_CLIENT_SECRET=223063904227@developer.gserviceaccount.com
 export STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage'
 export STATIC_URL='/static/'
