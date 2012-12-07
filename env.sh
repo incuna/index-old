3cd24842a7538e986fec4fded0ee6a8c00c2792b
George Hickman <george@ghickman.co.uk>
2012-12-07 14:22:30 +0000

    Add bucket name to env vars.

--- env.sh
+++ env.sh
@@ -1,5 +1,6 @@
 export DEBUG=True
 export DEVELOPMENT_SITE=True
 export EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
+export AWS_STORAGE_BUCKET_NAME='index.incuna.com'
 export STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage'
 export STATIC_URL='/static/'
